fb457198f1b0cdaad814e1383a9d9fba3fc69936
Abdul <abdul@uitoux.in>
2020-01-13 11:55:51 +0530

    Update ModulePkgServiceProvider.php

--- src/ModulePkgServiceProvider.php
+++ src/ModulePkgServiceProvider.php
@@ -14,7 +14,7 @@ class ModulePkgServiceProvider extends ServiceProvider {
 		$this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 		$this->loadRoutesFrom(__DIR__ . '/routes/api.php');
 		$this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-		$this->loadViewsFrom(__DIR__ . '/views', 'module');
+		$this->loadViewsFrom(__DIR__ . '/views', 'module-pkg');
 		$this->publishes([
 			__DIR__ . '/public' => base_path('public'),
 		]);
