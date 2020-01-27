f38fd510308a659520d52ec4abf7568b93c269b8
Abdul <abdul@uitoux.in>
2020-01-27 11:44:32 +0530

    u

--- src/ModulePkgServiceProvider.php
+++ src/ModulePkgServiceProvider.php
@@ -17,6 +17,8 @@ class ModulePkgServiceProvider extends ServiceProvider {
 		$this->loadViewsFrom(__DIR__ . '/views', 'module-pkg');
 		$this->publishes([
 			__DIR__ . '/public' => base_path('public'),
+			__DIR__ . '/database/seeds/client' => 'database/seeds',
+			__DIR__ . '/config/config.php' => config_path('module-pkg.php'),
 		]);
 	}
 
